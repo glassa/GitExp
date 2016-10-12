07dd45ea359b256339c6cc3d3894d937eac57167
imdokdokhe <tonykslee1@gmail.com>
2016-10-12 16:13:01 -0700

    commit 1

---
+++ app/src/main/java/tcss450/uw/edu/gitexp/TonyActivity.java
@@ -0,0 +1,13 @@
+package tcss450.uw.edu.gitexp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TonyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tony);
+    }
+}
